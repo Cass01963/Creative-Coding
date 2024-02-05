a4a0771287ca95b80d3a16e7a84dfd8b95fb926a
Christian Carranza <126421622+Cass01963@users.noreply.github.com>
2024-02-05 08:55:57 -0500

    Add files via upload

---
+++ IMandala_Carranza/javascript/cheese.js
@@ -0,0 +1,89 @@
+function cheese(ctx4,mouseX,mouseY,canvas) { 
+
+// layer1/Path
+      ctx4.save();
+      ctx4.beginPath();
+      ctx4.moveTo(212.8, 62.0);
+      ctx4.lineTo(139.5, 34.0);
+      ctx4.bezierCurveTo(138.9, 35.6, 137.9, 37.1, 136.6, 38.3);
+      ctx4.bezierCurveTo(131.5, 43.3, 123.3, 43.3, 118.3, 38.2);
+      ctx4.bezierCurveTo(114.7, 34.5, 113.8, 29.2, 115.4, 24.7);
+      ctx4.lineTo(50.8, 0.0);
+      ctx4.lineTo(45.3, 74.0);
+      ctx4.lineTo(258.5, 162.4);
+      ctx4.lineTo(212.8, 62.0);
+      ctx4.closePath();
+      ctx4.fillStyle = "rgb(254, 225, 132)";
+      ctx4.fill();
+
+      // layer1/Path
+      ctx4.beginPath();
+      ctx4.moveTo(210.0, 221.9);
+      ctx4.bezierCurveTo(208.2, 220.0, 208.6, 216.5, 211.1, 214.1);
+      ctx4.bezierCurveTo(213.6, 211.6, 217.1, 211.2, 219.0, 213.1);
+      ctx4.lineTo(258.5, 162.4);
+      ctx4.lineTo(50.9, 0.6);
+      ctx4.lineTo(35.7, 20.0);
+      ctx4.bezierCurveTo(37.2, 20.7, 38.6, 21.6, 39.8, 22.8);
+      ctx4.bezierCurveTo(45.9, 29.0, 44.3, 40.6, 36.1, 48.6);
+      ctx4.bezierCurveTo(28.2, 56.3, 17.0, 57.8, 10.7, 52.1);
+      ctx4.lineTo(0.0, 65.8);
+      ctx4.lineTo(207.6, 227.7);
+      ctx4.lineTo(211.4, 222.8);
+      ctx4.bezierCurveTo(210.9, 222.6, 210.4, 222.3, 210.0, 221.9);
+      ctx4.closePath();
+      ctx4.fillStyle = "rgb(255, 211, 101)";
+      ctx4.fill();
+
+      // layer1/Ellipse
+      ctx4.beginPath();
+      ctx4.moveTo(123.6, 133.7);
+      ctx4.bezierCurveTo(119.7, 137.6, 113.3, 137.5, 109.5, 133.5);
+      ctx4.bezierCurveTo(105.6, 129.6, 105.6, 123.2, 109.6, 119.4);
+      ctx4.bezierCurveTo(113.6, 115.5, 119.9, 115.6, 123.8, 119.5);
+      ctx4.bezierCurveTo(127.6, 123.5, 127.6, 129.8, 123.6, 133.7);
+      ctx4.closePath();
+      ctx4.fillStyle = "rgb(235, 167, 82)";
+      ctx4.fill();
+
+      // layer1/Ellipse
+      ctx4.beginPath();
+      ctx4.moveTo(84.3, 68.3);
+      ctx4.bezierCurveTo(78.6, 73.9, 69.4, 73.8, 63.8, 68.0);
+      ctx4.bezierCurveTo(58.1, 62.3, 58.2, 53.1, 64.0, 47.5);
+      ctx4.bezierCurveTo(69.7, 41.8, 78.9, 41.9, 84.6, 47.7);
+      ctx4.bezierCurveTo(90.2, 53.4, 90.1, 62.6, 84.3, 68.3);
+      ctx4.closePath();
+      ctx4.fill();
+
+      // layer1/Ellipse
+      ctx4.beginPath();
+      ctx4.moveTo(203.5, 174.6);
+      ctx4.bezierCurveTo(195.7, 182.3, 183.1, 182.1, 175.4, 174.3);
+      ctx4.bezierCurveTo(167.7, 166.4, 167.8, 153.8, 175.7, 146.1);
+      ctx4.bezierCurveTo(183.5, 138.4, 196.1, 138.5, 203.8, 146.4);
+      ctx4.bezierCurveTo(211.5, 154.2, 211.4, 166.9, 203.5, 174.6);
+      ctx4.closePath();
+      ctx4.fill();
+
+      // layer1/Ellipse
+      ctx4.beginPath();
+      ctx4.moveTo(212.2, 91.7);
+      ctx4.bezierCurveTo(209.9, 93.9, 204.5, 92.1, 200.1, 87.7);
+      ctx4.bezierCurveTo(195.7, 83.3, 194.1, 77.8, 196.4, 75.6);
+      ctx4.bezierCurveTo(198.7, 73.3, 204.1, 75.1, 208.4, 79.5);
+      ctx4.bezierCurveTo(212.8, 84.0, 214.5, 89.4, 212.2, 91.7);
+      ctx4.closePath();
+      ctx4.fill();
+
+      // layer1/Ellipse
+      ctx4.beginPath();
+      ctx4.moveTo(166.9, 77.3);
+      ctx4.bezierCurveTo(164.6, 79.5, 160.0, 78.4, 156.5, 74.8);
+      ctx4.bezierCurveTo(153.0, 71.2, 151.9, 66.5, 154.2, 64.3);
+      ctx4.bezierCurveTo(156.4, 62.2, 161.0, 63.3, 164.5, 66.9);
+      ctx4.bezierCurveTo(168.0, 70.5, 169.1, 75.1, 166.9, 77.3);
+      ctx4.closePath();
+      ctx4.fill();
+      ctx4.restore();
+    }
---
+++ IMandala_Carranza/javascript/spacehelmet.js
@@ -0,0 +1,150 @@
+function spacehelmet(ctx7,mouseX,mouseY,canvas) {
+
+var gradient;
+
+      // layer1/Path
+      ctx7.save();
+      ctx7.beginPath();
+      ctx7.moveTo(131.9, 41.2);
+      ctx7.bezierCurveTo(131.9, 41.2, 139.3, 31.9, 135.9, 25.0);
+      ctx7.bezierCurveTo(132.4, 18.2, 125.4, 19.0, 126.1, 14.4);
+      ctx7.bezierCurveTo(126.8, 9.7, 123.3, 0.4, 110.4, 4.5);
+      ctx7.bezierCurveTo(72.8, 16.6, 33.0, 55.6, 21.5, 73.6);
+      ctx7.bezierCurveTo(12.9, 87.3, 6.5, 101.0, 4.4, 114.6);
+      ctx7.bezierCurveTo(-1.7, 154.0, 24.0, 153.6, 36.1, 146.1);
+      ctx7.bezierCurveTo(48.1, 138.7, 131.9, 41.2, 131.9, 41.2);
+      ctx7.closePath();
+      ctx7.fillStyle = "rgb(34, 34, 34)";
+      ctx7.fill();
+      ctx7.lineWidth = 7.0;
+      ctx7.strokeStyle = "rgb(57, 56, 63)";
+      ctx7.lineCap = "round";
+      ctx7.lineJoin = "round";
+      ctx7.stroke();
+
+      // layer1/Path
+      ctx7.beginPath();
+      ctx7.moveTo(123.9, 16.8);
+      ctx7.bezierCurveTo(123.9, 16.8, 115.9, 19.8, 109.1, 22.9);
+      ctx7.bezierCurveTo(98.6, 27.6, 82.8, 36.6, 73.6, 47.6);
+      ctx7.stroke();
+
+      // layer1/Path
+      ctx7.beginPath();
+      ctx7.moveTo(12.6, 145.4);
+      ctx7.bezierCurveTo(12.6, 145.4, 11.5, 133.7, 14.6, 121.9);
+      ctx7.bezierCurveTo(17.5, 110.6, 24.3, 93.4, 36.6, 84.0);
+      ctx7.stroke();
+
+      // layer1/Line
+      ctx7.beginPath();
+      ctx7.moveTo(55.1, 62.9);
+      ctx7.lineTo(44.7, 73.2);
+      ctx7.stroke();
+
+      // layer1/Path
+      ctx7.beginPath();
+      ctx7.moveTo(162.5, 48.8);
+      ctx7.bezierCurveTo(162.5, 48.8, 144.0, 30.3, 132.9, 29.5);
+      ctx7.bezierCurveTo(123.0, 28.8, 83.4, 52.2, 61.0, 73.8);
+      ctx7.bezierCurveTo(38.0, 95.8, 30.2, 113.6, 28.9, 125.2);
+      ctx7.bezierCurveTo(27.4, 138.2, 29.1, 148.9, 32.5, 150.9);
+      ctx7.bezierCurveTo(35.8, 152.8, 71.7, 165.8, 71.7, 165.8);
+      ctx7.lineTo(162.5, 48.8);
+      ctx7.closePath();
+      ctx7.fillStyle = "rgb(241, 239, 247)";
+      ctx7.fill();
+      ctx7.stroke();
+
+      // layer1/Ellipse
+      ctx7.beginPath();
+      ctx7.moveTo(86.6, 214.3);
+      ctx7.bezierCurveTo(48.6, 197.8, 28.4, 160.3, 41.3, 130.6);
+      ctx7.bezierCurveTo(54.2, 100.8, 95.4, 90.0, 133.4, 106.5);
+      ctx7.bezierCurveTo(171.3, 122.9, 191.6, 160.4, 178.7, 190.2);
+      ctx7.bezierCurveTo(165.8, 220.0, 124.5, 230.8, 86.6, 214.3);
+      ctx7.closePath();
+      ctx7.save();
+      ctx7.transform(-0.398, 0.917, 0.917, 0.398, 442.6, 73.4);
+      gradient = ctx7.createLinearGradient(156.6, -242.1, 267.8, -299.0);
+      gradient.addColorStop(0.00, "rgb(101, 202, 227)");
+      gradient.addColorStop(0.29, "rgb(78, 140, 194)");
+      gradient.addColorStop(0.50, "rgb(55, 78, 161)");
+      gradient.addColorStop(0.79, "rgb(133, 75, 157)");
+      gradient.addColorStop(1.00, "rgb(211, 72, 154)");
+      ctx7.fillStyle = gradient;
+      ctx7.fill();
+      ctx7.stroke();
+
+      // layer1/Path
+      ctx7.restore();
+      ctx7.beginPath();
+      ctx7.moveTo(170.8, 212.1);
+      ctx7.bezierCurveTo(170.8, 212.1, 158.7, 214.5, 156.6, 213.2);
+      ctx7.bezierCurveTo(154.5, 211.9, 156.7, 193.4, 154.3, 177.2);
+      ctx7.bezierCurveTo(152.2, 162.5, 142.6, 140.5, 139.8, 137.5);
+      ctx7.bezierCurveTo(136.3, 133.5, 132.0, 135.0, 126.6, 134.5);
+      ctx7.bezierCurveTo(118.3, 133.7, 108.6, 121.7, 108.3, 106.7);
+      ctx7.bezierCurveTo(108.0, 90.5, 150.1, 28.8, 157.3, 27.4);
+      ctx7.bezierCurveTo(168.5, 25.2, 203.4, 63.8, 203.4, 63.8);
+      ctx7.lineTo(170.8, 212.1);
+      ctx7.closePath();
+      ctx7.fillStyle = "rgb(211, 210, 216)";
+      ctx7.fill();
+      ctx7.stroke();
+
+      // layer1/Ellipse
+      ctx7.beginPath();
+      ctx7.moveTo(129.6, 124.7);
+      ctx7.bezierCurveTo(120.3, 120.7, 116.1, 109.9, 120.1, 100.7);
+      ctx7.bezierCurveTo(124.1, 91.5, 134.8, 87.2, 144.1, 91.2);
+      ctx7.bezierCurveTo(153.3, 95.2, 157.5, 106.0, 153.5, 115.2);
+      ctx7.bezierCurveTo(149.5, 124.4, 138.8, 128.7, 129.6, 124.7);
+      ctx7.closePath();
+      ctx7.save();
+      ctx7.transform(-0.398, 0.917, 0.917, 0.398, 442.6, 73.4);
+      gradient = ctx7.createLinearGradient(143.8, -257.7, 170.3, -282.7);
+      gradient.addColorStop(0.14, "rgb(255, 242, 58)");
+      gradient.addColorStop(0.35, "rgb(246, 173, 59)");
+      gradient.addColorStop(0.51, "rgb(237, 104, 60)");
+      gradient.addColorStop(0.57, "rgb(234, 83, 59)");
+      gradient.addColorStop(1.00, "rgb(232, 62, 57)");
+      ctx7.fillStyle = gradient;
+      ctx7.fill();
+      ctx7.stroke();
+
+      // layer1/Path
+      ctx7.restore();
+      ctx7.beginPath();
+      ctx7.moveTo(82.7, 197.4);
+      ctx7.bezierCurveTo(104.2, 207.9, 123.2, 212.6, 137.7, 203.7);
+      ctx7.lineWidth = 9.0;
+      ctx7.strokeStyle = "rgb(255, 254, 255)";
+      ctx7.stroke();
+
+      // layer1/Line
+      ctx7.beginPath();
+      ctx7.moveTo(72.5, 190.1);
+      ctx7.lineTo(60.3, 180.7);
+      ctx7.stroke();
+
+      // layer1/Path
+      ctx7.beginPath();
+      ctx7.moveTo(165.2, 207.0);
+      ctx7.bezierCurveTo(165.3, 211.1, 169.3, 213.4, 173.1, 211.8);
+      ctx7.bezierCurveTo(183.5, 207.6, 192.8, 201.9, 203.1, 190.1);
+      ctx7.bezierCurveTo(210.1, 182.0, 223.3, 158.5, 226.3, 144.7);
+      ctx7.bezierCurveTo(229.8, 128.8, 227.8, 109.8, 225.4, 99.7);
+      ctx7.bezierCurveTo(220.8, 80.8, 207.3, 62.1, 200.3, 63.3);
+      ctx7.bezierCurveTo(193.4, 64.4, 185.8, 114.9, 178.2, 125.6);
+      ctx7.bezierCurveTo(170.6, 136.2, 164.3, 147.7, 162.8, 156.0);
+      ctx7.bezierCurveTo(161.3, 164.2, 166.7, 178.9, 166.6, 189.7);
+      ctx7.bezierCurveTo(166.5, 194.1, 165.0, 200.1, 165.2, 207.0);
+      ctx7.closePath();
+      ctx7.fillStyle = "rgb(238, 231, 193)";
+      ctx7.fill();
+      ctx7.lineWidth = 7.0;
+      ctx7.strokeStyle = "rgb(57, 56, 63)";
+      ctx7.stroke();
+      ctx7.restore();
+    }
